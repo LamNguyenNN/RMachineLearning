5436b5c690a81bcc83d00b5140beaa4398cde861
LamNguyenNN <lamnguyen1029@hotmail.com>
2019-01-19 22:08:36 -0700

    Finished forward propagation and error calculation

---
+++ ANN.R
@@ -0,0 +1,89 @@
+initWeightMats = function(topo) {
+  i = 1
+  weightMats = list()
+  while(i < length(topo)) {
+    weightMats[[i]] = matrix(data = rnorm(n=topo[i]*topo[i+1], mean=0, sd=1), nrow=topo[i], ncol=topo[i+1], byrow=T)
+    i = i + 1
+  }
+  return (weightMats)
+}
+
+initBiasMats = function(topo, numInputSets) {
+  i = 1
+  biasMats = list()
+  while(i < length(topo)) {
+    biasMats[[i]] = matrix(data = rnorm(n=numInputSets*topo[i+1], mean=0, sd=1), nrow=numInputSets, ncol=topo[i+1], byrow=T)
+    i = i + 1
+  }
+  return (biasMats)
+}
+
+sigmoid = function(x) {
+  1 / (1 + exp(-x))
+}
+
+elu = function(x) {
+  ifelse(x > 0, x, eluAlpha * (exp(x) - 1))
+   
+}
+
+softmax = function(x) {
+  sum = sum(exp(x))
+  exp(x)/sum
+}
+
+forwardProp = function(inputMat, weightMats, biasMats) {
+  numSynapses = length(weightMats)
+  
+  activatedSums = list()
+  
+  activatedSums[[1]] = elu((inputMat %*% weightMats[[1]]) + biasMats[[1]])
+  
+  i = 2
+  while(i < numSynapses) {
+    activatedSums[[i]] = elu((activatedSums[[i-1]] %*% weightMats[[i]]) + biasMats[[i]])
+    i = i + 1
+  }
+  
+  output = (activatedSums[[numSynapses-1]] %*% weightMats[[numSynapses]]) 
+                                       + biasMats[[numSynapses]]
+  
+  for(i in 1:nrow(output)) {
+    output[i,] = softmax(output[i,])
+  }
+  
+  return (list("activatedSums" = activatedSums, "output" = output))
+}
+
+MSECost = function(targetOutput, netOutput) {
+  error = (1/nrow(targetOutput)) * sum( (1/ncol(targetOutput))*(rowSums((targetOutput - netOutput) ^ 2)) )
+  return(error)
+}
+
+CrossEntropyCost = function(targetOutput, netOutput) {
+  error = (1/nrow(targetOutput)) * sum( rowSums(-targetOutput * log(netOutput)) )
+  return(error)
+}
+
+SGD = function(targetOutput, netOutput) {
+  
+}
+
+input = matrix(data = c(0,0, 1,0, 0,1, 1,1), nrow = 4, ncol = 2, byrow = T)
+trainOutput = matrix(data = c(1,0, 0,1, 0,1, 1,0), nrow = 4, ncol = 2, byrow = T)
+
+numInputSets = 4
+numLayers = 3
+eluAlpha = 1
+topology = c(2,3,3,2)
+
+weightMats = initWeightMats(topology)
+biasMats = initBiasMats(topology, numInputSets)
+
+output = forwardProp(input, weightMats, biasMats)
+output
+
+CrossEntropyCost(trainOutput, output$output)
+
+
+
